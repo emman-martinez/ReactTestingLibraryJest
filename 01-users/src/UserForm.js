f83b72f8bc3d299d4a8ef491433f20ac6183ecc1
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-06-05 12:13:23 -0600

    07-Adding the Form

---
+++ 01-users/src/UserForm.js
@@ -0,0 +1,17 @@
+function UserForm() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input />
+      </div>
+      <div>
+        <label htmlFor="name">Email</label>
+        <input />
+      </div>
+      <button>Add User</button>
+    </form>
+  );
+}
+
+export default UserForm;
