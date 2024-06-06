dc92071b2a318e8ab00b18f31012d924fdcdcaec
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-06-06 12:55:45 -0600

    20-Testing the UserList

---
+++ 01-users/src/UserList.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import UserList from "./UserList";
+
+test("render one row per user", () => {
+  // Render the component
+  const users = [
+    { name: "jane", email: "jane@jane.com" },
+    { name: "sam", email: "sam@sam.com" },
+  ];
+
+  render(<UserList users={users} />);
+
+  // Find all the rows in the table
+  // Assertion: correct number of rows in the table
+});
+
+test("render the email and name of each user", () => {});
