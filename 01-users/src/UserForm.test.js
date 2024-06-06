3609144b9f13267133cdcf10687c1a323744697e
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-06-06 12:26:06 -0600

    18-Introducing Mock Functions

--- 01-users/src/UserForm.test.js
+++ 01-users/src/UserForm.test.js
@@ -48,11 +48,28 @@ test("NOT THE BEST IMPLEMENTATION: it calls the onUserAdd when the form is submi
 
 test("THE BEST IMPLEMENTATION: it calls the onUserAdd when the form is submitted", () => {
   // THE BEST IMPLEMENTATION
+  const mock = jest.fn();
   // Try to render my component
+  render(<UserForm onUserAdd={mock} />);
+
   // Find the two inputs
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
   // Simulate typing in a name
+  user.click(nameInput);
+  user.keyboard("jane");
+
   // Simulate typing in an email
+  user.click(emailInput);
+  user.keyboard("jane@jane.com");
+
   // Find the button
+  const button = screen.getByRole("button");
+
   // Simulate clicking the button
+  user.click(button);
+
   // Assertion to make sure 'onUserAdd' gets called with email/name
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: "jane", email: "jane@jane.com" });
 });
