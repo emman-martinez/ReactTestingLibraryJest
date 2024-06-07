e7297ed4271ec5afd2a92b6318acb4e152823130
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-06-07 11:23:43 -0600

    A Touch of Test Driven Development

--- 01-users/src/UserForm.test.js
+++ 01-users/src/UserForm.test.js
@@ -74,3 +74,21 @@ test("THE BEST IMPLEMENTATION: it calls the onUserAdd when the form is submitted
   expect(mock).toHaveBeenCalled();
   expect(mock).toHaveBeenCalledWith({ name: "jane", email: "jane@jane.com" });
 });
+
+test("empties the two inputs when form is submitted", () => {
+  render(<UserForm onUserAdd={() => {}} />);
+
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+  const button = screen.getByRole("button");
+
+  user.click(nameInput);
+  user.keyboard("jane");
+  user.click(emailInput);
+  user.keyboard("jane@jane.com");
+
+  user.click(button);
+
+  expect(nameInput).toHaveValue("");
+  expect(emailInput).toHaveValue("");
+});
