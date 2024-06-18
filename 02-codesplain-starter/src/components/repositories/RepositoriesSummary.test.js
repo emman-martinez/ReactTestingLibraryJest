1b75d206d0d9685f313f660ee4a098e1edeae763
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-06-18 17:42:18 -0600

    59-Flexible Queries

--- 02-codesplain-starter/src/components/repositories/RepositoriesSummary.test.js
+++ 02-codesplain-starter/src/components/repositories/RepositoriesSummary.test.js
@@ -4,8 +4,8 @@ import RepositoriesSummary from "./RepositoriesSummary";
 const repository = {
   language: "JavaScript",
   stargazers_count: 5,
-  open_issues: 1,
   forks: 30,
+  open_issues: 1,
 };
 test("displays the primary language of the repository", () => {
   render(<RepositoriesSummary repository={repository} />);
@@ -15,12 +15,12 @@ test("displays the primary language of the repository", () => {
   expect(language).toBeInTheDocument();
 });
 
-test("displays", () => {
+test("displays information about the repository", () => {
   render(<RepositoriesSummary repository={repository} />);
 
   for (let key in repository) {
     const value = repository[key];
-    const element = screen.getByText(value);
+    const element = screen.getByText(new RegExp(value));
     expect(element).toBeInTheDocument();
   }
 });
