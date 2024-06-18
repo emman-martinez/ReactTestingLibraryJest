f97736b434b3fffd96010be9f7959953142e2a1e
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-06-18 17:36:49 -0600

    58-Looping Over Assertions

--- 02-codesplain-starter/src/components/repositories/RepositoriesSummary.test.js
+++ 02-codesplain-starter/src/components/repositories/RepositoriesSummary.test.js
@@ -1,17 +1,26 @@
 import { screen, render } from "@testing-library/react";
 import RepositoriesSummary from "./RepositoriesSummary";
 
+const repository = {
+  language: "JavaScript",
+  stargazers_count: 5,
+  open_issues: 1,
+  forks: 30,
+};
 test("displays the primary language of the repository", () => {
-  const repository = {
-    language: "JavaScript",
-    stargazers_count: 5,
-    open_issues: 1,
-    forks: 30,
-  };
-
   render(<RepositoriesSummary repository={repository} />);
 
   const language = screen.getByText("JavaScript");
 
   expect(language).toBeInTheDocument();
+});
+
+test("displays", () => {
+  render(<RepositoriesSummary repository={repository} />);
+
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(value);
+    expect(element).toBeInTheDocument();
+  }
 });
