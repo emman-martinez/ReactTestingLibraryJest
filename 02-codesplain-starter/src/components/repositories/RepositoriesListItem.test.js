f5cb0159dd1b3e27ff47cf3c2c8b9b54c49e1ed4
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-06-19 10:34:27 -0600

    67-Module Mocks

--- 02-codesplain-starter/src/components/repositories/RepositoriesListItem.test.js
+++ 02-codesplain-starter/src/components/repositories/RepositoriesListItem.test.js
@@ -2,6 +2,13 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import RepositoriesListItem from "./RepositoriesListItem";
 
+jest.mock("../tree/FileIcon", () => {
+  // Content of FileIcon.js
+  return () => {
+    return "File Icon Component";
+  };
+});
+
 function renderComponent() {
   const repository = {
     full_name: "facebook/react",
@@ -22,7 +29,7 @@ function renderComponent() {
 test("shows a link to the github homepage for this repository", async () => {
   renderComponent();
 
-  await screen.findByRole("img", { name: /javascript/i });
+  // await screen.findByRole("img", { name: /javascript/i });
 
   // screen.debug();
   // await pause();
